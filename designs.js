bf7408576d7baa3a988ea694fd20313a63f288b4
Matt <m.s.morrill@gmail.com>
2018-03-05 07:53:51 -0500

    Added variables, click() event, and id=submitBox

--- designs.js
+++ designs.js
@@ -1,6 +1,8 @@
-// Select color input
-// Select size input
+var color = $( "#colorPicker" ).val();
+var height = $( '#inputHeight' ).val();
+var width = $( '#inputWidth' ).val();
 
+$( '#submitBox' ).click( makeGrid() );
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
