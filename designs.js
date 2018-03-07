780b7ef5fc3bbc1f5573133ad5a557daa3c20193
Matt <m.s.morrill@gmail.com>
2018-03-06 19:20:12 -0500

    Changed the code for the color Picker and also tried to add a way to draw while dragging the mouse.

--- designs.js
+++ designs.js
@@ -1,12 +1,40 @@
 var color = $( "#colorPicker" ).val();
-var height = $( '#inputHeight' ).val();
-var width = $( '#inputWidth' ).val();
+var mouseDown = 0;
+document.body.onmousedown = function() {
+  mouseDown = 1;
+};
+document.body.onmouseup = function() {
+  mouseDown = 0;
+};
 
-$( '#submitBox' ).click( makeGrid() );
-// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+  $("sizePicker").submit(function makeGrid(grid) {
+    $('table tr').remove();
+    var rows = $( '#inputHeight' ).val();
+    var columns = $( '#inputWeight' ).val();
+    for (var c = 1; columns; c++) {
+      $("pixelCanvas").append('<tr></tr>');
+      for (var r = 1; rows; r++) {
+        $("tr").last().append('<td></td>');
+      }
+    }
+    $("pixelCanvas").css("border", "black")
+    grid.preventDefault();
+}));
 
-function makeGrid() {
+// Select color input
+$("#colorPicker").on("change", function(){
+  var color = $("#colorPicker.val()")
+});
 
-// Your code goes here!
+//set color
+$("#pixelCanvas").on("click","td", function(){
+  var color = $("colorPicker.val()");
+  $(this).css("background-color", color);
+});
 
-}
+$("td").mouseenter(function(){
+  if(mouseDown){
+    $(this).css("background-color", colorPicker.value);
+  }
+});
